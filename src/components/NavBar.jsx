e60f5981ff2969d18030c0e7972e25ae80da76d0
leovoon <voonlihhaur@gmail.com>
2020-09-02 13:26:12 +0800

    initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+function NavBar() {
+    return (
+        <div>
+        <NavLink exact to="/">Home</NavLink>            
+        <NavLink to="/about">About</NavLink>            
+        </div>
+
+)
+}
+
+export default NavBar
